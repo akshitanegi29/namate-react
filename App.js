2dd305fff30dab3602ae6be9df15d2ac8ed1f89c
Akshita Negi <akshitanegi@192.168.1.11>
2023-10-21 23:30:07 +0530

    lesson 1

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement('div', {id: 'parent'}, React.createElement('h1', {}, 'Helle React'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const root2 = ReactDOM.createRoot(document.getElementById('aboveRoot'));
+root2.render(React.createElement('h1',null,'above root child'))
+root.render(heading); 
