6295fbdb800b2d3f20b9db35d0b6514939d7b9f9
akshitanegi29 <akshita29negi@gmail.com>
2023-11-27 14:38:57 +0530

    lesson 4.1: laying foundation- Dummy food ordering app

--- App.js
+++ App.js
@@ -1,21 +1,87 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//const heading = React.createElement('div', {id: 'parent'}, React.createElement('h1', {}, 'Hello React'));
+/**
+ * Header
+ *  -Logo
+ *  -Nav Items
+ * Body
+ *  -Search
+ *  -RestaurantCountainer
+ *  -RestaurantCard
+ *      -img
+ *      -name of res, star rating, cuisines, Eta
+ * Footer
+ *  -Copyright
+ *  -Links
+ *  -Address
+ *  -Contact
+ */
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='logo-container'>
+                <img className='logo' src='https://www.vhv.rs/dpng/d/459-4595986_cartoon-gourmet-cute-burger-png-and-vector-image.png'></img>
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
 
-// React Element
-const jsxheading = <div id = "parent">
-    <h1>Hello React Element</h1>
-</div>
+    )
+}
+const RestaurantCard = () => {
+    return (
+        <div className='res-card' style={{backgroundColor: '#f0f0f0'}}>
+            <img className='res-card-img'
+            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSamP2LsWlWFsjWPB9M8HBKoIhPH3VXNknNWg&usqp=CAU'></img>
+            <h3>Dominos</h3>
+            <h4>Italian, Continental</h4>
+            <h4>4.3 stars</h4>
+            <h4>38 minutes</h4>
+        </div>
+    )
+}
+const Body = () => {
+    return (
+        <div className='body'>
+            <div className='search'> Search </div>
+            <div className='res-container'>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+        </div> 
+    )
+}
+const Footer = () => {
+    return (
+        <div className='footer-container'>
+            <a>Copyright |</a>
+            <a href=''>Place order via zomato </a>
+            <h4>| Address: Manhattan, US |</h4>
+            <h4>Akshita Negi |</h4>
+        </div>
+    )
+}
+const AppLayout = () => {
+    return (
+        <div className='app'>
+            <Header />
+            <Body/>
+            <Footer/>
+        </div>
+    )
+}
 
-// React functional component
-const HeadingComponent = () => ( //skipped return as this functions job is only to return one element
-    <div id = "parent">
-        {jsxheading}
-        <h1>Hello React component</h1>
-    </div>
-);
-//jsxHeading is a variable.
-const root = ReactDOM.createRoot(document.getElementById('root'));
-//root.render(jsxheading); 
-root.render(<HeadingComponent/>);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<AppLayout/>);
