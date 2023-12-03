c2de4b8f6c30759732e2d10a4ac657a492b740ea
akshitanegi29 <akshita29negi@gmail.com>
2023-12-03 23:47:17 +0530

    lesson 6 - useEffect()

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,35 +1,53 @@
 import RestaurantCard from "./RestaurantCard";
-import restList from "../../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-    const [restaurantList, setRestaurantList] = useState(restList);
-    return (
-        <div className='body'>
-            <div className='filter'>
-                <button className="filter-btn"
-                onClick={()=>{
-                    const filteredList = restaurantList.filter(
-                        rest => rest.data.avgRating > 4
-                    )
-                    setRestaurantList(filteredList);
-                }}
-                >
-                    Top Rated Restaurants
-                </button>
-            </div>
-            <div className='res-container'>
-                {
-                    restaurantList.map(
-                        restaurant => <RestaurantCard 
-                        key = {restaurant.data.id}
-                        restData = {restaurant}
-                        />
-                        )
-                }
-            </div>
-        </div> 
-    )
-}
+  const [restaurantList, setRestaurantList] = useState([]);
+
+  //callback func of useEffect gets called after the comp has rendered
+  useEffect(() => {
+    fetchData();
+  });
+
+  //API call
+  const fetchData = async () => {
+    const restData = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await restData.json();
+    // Optional Chaining
+    setRestaurantList(
+      json?.data?.cards[2]?.card?.card?.gridElements.infoWithStyle.restaurants
+    );
+  };
+
+  if (restaurantList.length === 0) {
+    return <Shimmer />;
+  }
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = restaurantList.filter(
+              (rest) => rest.info.avgRating > 4
+            );
+            setRestaurantList(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {restaurantList.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} restData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
