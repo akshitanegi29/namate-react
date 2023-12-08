77dfafb2aa09e2f6a988b5fc06d81d84d3f13394
akshitanegi29 <akshita29negi@gmail.com>
2023-12-09 00:46:13 +0530

    lesson: 7: Basic routing added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
-
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const AppLayout = () => {
     return (
@@ -15,5 +18,22 @@ const AppLayout = () => {
     )
 }
 
+const routeConfig = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout/>,
+        errorElement: <Error/>
+    },
+    {
+        path: "/About",
+        element: <About/>
+    },
+    {
+        path: "/Contact",
+        element: <Contact/>
+    }
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<AppLayout/>);
+//root.render(<AppLayout/>);
+root.render(<RouterProvider router = {routeConfig}/>)
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <h1> Hola Foodies!
+            </h1>
+            <h3>You are in the right place to kill your apetite :)</h3>
+            <h4>Bon Apetite!</h4>
+        </div>
+    )
+}
+export default About;
