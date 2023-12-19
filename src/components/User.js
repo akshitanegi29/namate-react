14c54dbb3b9c3e2ca45219eae17c001b3127e4af
akshitanegi29 <akshita29negi@gmail.com>
2023-12-19 20:26:22 +0530

    lesson 8: componentDidMount

--- src/components/User.js
+++ src/components/User.js
@@ -1,20 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const User = (props) => {
   const [count, setCount] = useState(0);
+  const [userInfo, setUserInfo] = useState({});
+
   const { name, location } = props;
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  async function getUserData() {
+    const data = await fetch("https://api.github.com/users/akshitanegi29");
+    const json = await data.json();
+    setUserInfo(json);
+  }
+
   return (
     <div className="user-card">
+      <img src={userInfo.avatar_url}></img>
+      <h2>Name: {userInfo.name}</h2>
+      <p>Address: {userInfo.html_url}</p>
       <button
         onClick={() => {
-          setCount(count+1);
+          setCount(count + 1);
         }}
       >
         Click me!
       </button>
-      <span> Count {count}</span>
-      <h2>Name: {name}</h2>
-      <p>Address: {location}</p>
     </div>
   );
 };
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -7,14 +7,26 @@ class UserClass extends React.Component {
     this.state = {
       count0: 0,
       count1: 1,
+      userInfo: {},
     };
   }
+
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/akshitanegi29");
+    const json = await data.json();
+    this.setState({ userInfo: json });
+  }
+
   render() {
     const { name, location } = this.props;
     const { count0, count1 } = this.state;
+    const { login, avatar_url, html_url } = this.state.userInfo;
 
     return (
       <div className="user-card">
+        <img src={avatar_url}></img>
+        <h2>Name(class): {login}</h2>
+        <p>Address: {html_url}</p>
         <button
           onClick={() => {
             this.setState({
@@ -22,10 +34,9 @@ class UserClass extends React.Component {
             });
           }}
         >
-          Click me! 
-        </button><span> Count {count0}</span>
-        <h2>Name(class): {name}</h2>
-        <p>Address: {location}</p>
+          Click me!
+        </button>
+        <span> Count {count0}</span>
       </div>
     );
   }
